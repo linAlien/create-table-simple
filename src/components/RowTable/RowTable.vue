f1e41f4ebb1e2d8e3e9a90d885fae107a5bef2e8
linAlien <hellohello126@yahoo.com>
2020-07-27 14:15:10 +0800

    updata

--- src/components/RowTable/RowTable.vue
+++ src/components/RowTable/RowTable.vue
@@ -16,6 +16,7 @@
 
   </div>
 </template>
+
 <script>
 
 import TableRowHead from "./TableRowHead.vue";
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+
 var path = require('path')
 var webpack = require('webpack')
 
@@ -11,8 +12,8 @@ module.exports = {
     publicPath: '/dist/',
     filename: 'CreateTable.js',
     library: "CreateTable",
-    libraryTarget: "umd",
-    umdNamedDefine: true,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   module: {
     rules: [
@@ -22,11 +23,41 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ],
-      },      {
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader?indentedSyntax'
+        ],
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
           loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+            'sass': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
           }
           // other vue-loader options go here
         }
